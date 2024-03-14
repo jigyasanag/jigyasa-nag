39727edfe46300c29f5820e84024239a3ac642dc
jigyasanag <163386447+jigyasanag@users.noreply.github.com>
2024-03-14 12:47:24 +0530

    Add files via upload

---
+++ veriable.js
@@ -0,0 +1,8 @@
+const input = require("readline-sync");
+let a =input.questionInt("enter the date ");
+let b= input.questionInt("enter the month");
+let c = input.questionInt("enter the year ");
+
+console.log( a);
+console.log(b);
+console.log(c);
